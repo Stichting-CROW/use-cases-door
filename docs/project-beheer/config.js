6dbb08cc700efaf3e4cb7479d3833943b752aff9
Elisabeth de Vries <48585720+ElisabethDeVries@users.noreply.github.com>
2025-06-04 10:54:50 +0200

    update report

--- docs/project-beheer/config.js
+++ docs/project-beheer/config.js
@@ -92,5 +92,4 @@ var respecConfig = {
   github: "https://github.com/Stichting-CROW/use-cases-door",
   postProcess: [indexOfRequirements],
   hypothesisComments: false,
-reviewDateEnd: 2025-07-09,
 };
